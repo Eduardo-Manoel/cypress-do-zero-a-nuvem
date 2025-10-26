94a6cda6c076852e2d3fb05b15b8b31f7a50c4d0
Eduardo Manoel <107643121+Eduardo-Manoel@users.noreply.github.com>
2025-10-26 11:34:56 -0300

    Alteracao Projeto

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,7 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('Verifica o titulo da aplicacao', () => {
+    cy.visit('./src/index.html')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    // Should e para verificar verificação imediata//
+  })
+})
