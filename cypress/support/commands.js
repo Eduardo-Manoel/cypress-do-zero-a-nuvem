10598d2005f92afaeb5164d5f62b18171c2199ab
Eduardo-Manoel <eduardomanoelnn@yahoo.com.br>
2025-11-03 12:31:55 -0300

    Aulas 4 e 5 Finalizadas

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,9 +5,14 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
     email: 'eduardomanoelnn@yahoo.com.br',
     text: longtext
 }) => {
-    cy.get('#firstName').type(data.firstName)
-    cy.get('#lastName').type(data.lastName)
-    cy.get('#email').type(data.email)
-    cy.get('#open-text-area').type(data.text, { delay: 0 })
-    cy.contains('button','Enviar').click()
+    cy.get('#firstName')
+        .type(data.firstName)
+    cy.get('#lastName')
+        .type(data.lastName)
+    cy.get('#email')
+        .type(data.email)
+    cy.get('#open-text-area')
+        .type(data.text, { delay: 0 })
+    cy.contains('button', 'Enviar')
+        .click()
 })
