149e6ed4952a12ee9b241aca05128d97e619c468
Eduardo-Manoel <eduardomanoelnn@yahoo.com.br>
2025-12-02 18:11:40 -0300

    Atualizacao do Cypress para nova versão e testando a aplicacao com o navegador opera

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,25 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
   video: true,
-  screenshots: true
+  screenshots: true,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Pega versão exata do Opera para usar como string
+      const operaPath = 'C:\\Users\\eduar\\AppData\\Local\\Programs\\Opera\\opera.exe';
+
+      config.browsers.push({
+        name: 'opera',
+        channel: 'stable',
+        family: 'chromium',
+        displayName: 'Opera',
+        path: operaPath,
+        version: '124.0.5705.65',  // <-- MUDE AQUI: string obrigatória
+        majorVersion: 124      // Mantenha para compatibilidade
+      });
+
+      return config;
+    },
+  },
 },
 )
